385c56221b4bc4c47ccf8e7d2f219dd076e9f9f1
Brandon <brandon.howar87@gmail.com>
2021-06-18 02:33:23 -0400

    fixed styling

--- hacker.js
+++ hacker.js
@@ -1,7 +1,7 @@
-$("body").keypress(function (e) {
+/* $("body").keypress(function (e) {
   var key = e.which;
   if (key == 13) {
     // the enter key code
     $(".typing-text").css("display", "block");
   }
-});
+}) */
